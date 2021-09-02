22757408c1a7b7afae536c06c63fe23d9d1c1495
Keita Ito <kito@ux-xu.com>
2021-09-02 17:33:18 +0900

    feat: Update test code

--- test/test_api.py
+++ test/test_api.py
@@ -6,11 +6,12 @@ import time
 import unittest
 from emo_platform import api
 
-class TestClientPrintInfo(unittest.TestCase):
+refresh_token = "cd86f829-ebdd-4c74-aaad-7c7cb8896cfb"
 
-	def setUp(self):
-		refresh_token = "cd86f829-ebdd-4c74-aaad-7c7cb8896cfb"
-		self.client = api.Client(refresh_token)
+class TestClientPrintInfo(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.client = api.Client(refresh_token)
 
 	def test_get_account_info(self):
 		print("\n" + "="*20 + " account info " + "="*20)
@@ -32,17 +33,16 @@ class TestClientPrintInfo(unittest.TestCase):
 		print("\n" + "="*20 + " motions list " + "="*20)
 		print(self.client.get_motions_list())
 
-	def test_get_webhook_setting(self):
-		print("\n" + "="*20 + " webhook setting " + "="*20)
-		print(self.client.get_webhook_setting())
+	# def test_get_webhook_setting(self):
+	# 	print("\n" + "="*20 + " webhook setting " + "="*20)
+	# 	print(self.client.get_webhook_setting())
 
 class TestRoomPrintInfo(unittest.TestCase):
-
-	def setUp(self):
-		refresh_token = "1686113e-8bcf-4c1b-acbf-f47edc4840cc"
-		client = api.Client(refresh_token)
-		rooms_id_list = client.get_rooms_id()
-		self.room = client.create_room_client(rooms_id_list[0])
+	@classmethod
+	def setUpClass(cls):
+		cls.client = api.Client(refresh_token)
+		rooms_id_list = cls.client.get_rooms_id()
+		cls.room = cls.client.create_room_client(rooms_id_list[0])
 
 	def test_get_msgs(self):
 		print("\n" + "="*20 + " room msgs " + "="*20)
@@ -54,7 +54,7 @@ class TestRoomPrintInfo(unittest.TestCase):
 
 	def test_get_sensor_values(self):
 		print("\n" + "="*20 + " room sensor values " + "="*20)
-		sensor_list = self.room.get_sensors_list()
+		_, sensor_list = self.room.get_sensors_list()
 		print(self.room.get_sensor_values(sensor_list['sensors'][0]['uuid']))
 
 	def test_get_emo_settings(self):
@@ -62,29 +62,56 @@ class TestRoomPrintInfo(unittest.TestCase):
 		print(self.room.get_emo_settings())
 
 class TestRoomSendData(unittest.TestCase):
-
-	def setUp(self):
-		refresh_token = "1686113e-8bcf-4c1b-acbf-f47edc4840cc"
-		self.client = api.Client(refresh_token)
-		rooms_id_list = self.client.get_rooms_id()
-		self.room = self.client.create_room_client(rooms_id_list[0])
+	@classmethod
+	def setUpClass(cls):
+		cls.client = api.Client(refresh_token)
+		rooms_id_list = cls.client.get_rooms_id()
+		cls.room = cls.client.create_room_client(rooms_id_list[0])
 
 	# def test_send_audio_msg(self):
 	# 	print("\n" + "="*20 + " room send audio msg " + "="*20)
 	# 	audio_data_path = "../assets/sample_audio.mp3"
 	# 	print(self.room.send_audio_msg(audio_data_path))
 
-	def test_send_image(self):
-		pass
+	# def test_send_image(self):
+	# 	pass
 
+	# @unittest.skip("")
 	def test_send_msg(self):
 		print("\n" + "="*20 + " room send msg " + "="*20)
-		print(self.room.send_msg("おはよう"))
+		print(self.room.send_msg("お"*1))
 
+	# @unittest.skip("")
 	def test_send_stamp(self):
 		print("\n" + "="*20 + " room send all stamps " + "="*20)
-		stamp_list = self.client.get_stamps_list()
+		_, stamp_list = self.client.get_stamps_list()
 		for stamp in stamp_list['stamps']:
 			time.sleep(7) # for avoiding rate limit
 			print("\n" + "="*10 + " room send stamp " + "="*10)
 			print(self.room.send_stamp(stamp['uuid']))
+			break
+
+	# @unittest.skip("")
+	def test_send_original_motion(self):
+		print("\n" + "="*20 + " room send original motion " + "="*20)
+		audio_data_path = "../assets/sample_motion.json"
+		print(self.room.send_original_motion(audio_data_path))
+
+	# @unittest.skip("")
+	def test_change_led_color(self):
+		print("\n" + "="*20 + " room change led color " + "="*20)
+		print(self.room.change_led_color(api.Color(0, 100, 100)))
+
+	# @unittest.skip("")
+	def test_move_to(self):
+		print("\n" + "="*20 + " room move to " + "="*20)
+		print(self.room.move_to(api.Head(40, 20)))
+
+	# @unittest.skip("")
+	def test_send_motion(self):
+		print("\n" + "="*20 + " room send all motions " + "="*20)
+		_, motion_list = self.client.get_motions_list()
+		for motion in motion_list['motions']:
+			time.sleep(7) # for avoiding rate limit
+			print("\n" + "="*10 + " room send motion " + "="*10)
+			print(self.room.send_motion(motion['uuid']))
