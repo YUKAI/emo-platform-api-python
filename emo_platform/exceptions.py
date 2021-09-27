51c1d866d60a212e5b0a7507b2da1a2611ddd19f
Keita Ito <kito@ux-xu.com>
2021-09-27 11:12:37 +0900

    style: Add docstring to emo_platform.exceptions

--- emo_platform/exceptions.py
+++ emo_platform/exceptions.py
@@ -4,35 +4,51 @@ import requests
 
 
 class EmoPlatformError(Exception):
+    """BOCCO emo Platform API利用時のエラー"""
+
     def __init__(self, message):
         self.message = message
 
 
 class RateLimitError(EmoPlatformError):
+    """1分あたり10回のAPI利用回数を上回った場合に出るエラー"""
+
     pass
 
 
 class UnauthorizedError(EmoPlatformError):
+    """API利用に際しての認証エラー"""
+
     pass
 
 
 class NotFoundError(EmoPlatformError):
+    """指定したAPIのURLが存在しない場合に出るエラー"""
+
     pass
 
 
 class BadRequestError(EmoPlatformError):
+    """送るデータの形式が誤っている場合に出るエラー"""
+
     pass
 
 
 class UnknownError(EmoPlatformError):
+    """未定義のエラー"""
+
     pass
 
 
 class NoRoomError(EmoPlatformError):
+    """BOCCOアカウントに紐づいた部屋がない場合に出るエラー"""
+
     pass
 
 
 class NoRefreshTokenError(EmoPlatformError):
+    """リフレッシュトークンが正しく設定されてない場合に出るエラー"""
+
     pass
 
 
