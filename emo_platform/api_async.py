5803ff81c44e9e15546cfbeca01d535cf74b8309
Keita Ito <kito@ux-xu.com>
2021-10-04 15:00:34 +0900

    feat: Change async http request return value from dict to class

--- emo_platform/api_async.py
+++ emo_platform/api_async.py
@@ -15,6 +15,19 @@ from emo_platform.exceptions import (
     aiohttp_error_handler,
 )
 from emo_platform.models import Color, Head, WebHook
+from emo_platform.response import (
+    EmoAccountInfo,
+    EmoMessageInfo,
+    EmoMotionsInfo,
+    EmoMsgsInfo,
+    EmoRoomInfo,
+    EmoRoomSensorInfo,
+    EmoSensorsInfo,
+    EmoSettingsInfo,
+    EmoStampsInfo,
+    EmoTokens,
+    EmoWebhookInfo,
+)
 
 
 class AsyncClient(Client):
@@ -25,9 +38,9 @@ class AsyncClient(Client):
 
         if refresh_token != "":
             try:
-                refresh_token, self.access_token = await self._get_access_token(
-                    refresh_token
-                )
+                res_tokens = await self._get_access_token(refresh_token)
+                self.access_token = res_tokens.access_token
+                refresh_token = res_tokens.refresh_token
                 self.headers["Authorization"] = "Bearer " + self.access_token
                 tokens["refresh_token"] = refresh_token
                 tokens["access_token"] = self.access_token
@@ -49,9 +62,9 @@ class AsyncClient(Client):
                 )
 
             try:
-                refresh_token, self.access_token = await self._get_access_token(
-                    refresh_token
-                )
+                res_tokens = await self._get_access_token(refresh_token)
+                self.access_token = res_tokens.access_token
+                refresh_token = res_tokens.refresh_token
                 self.headers["Authorization"] = "Bearer " + self.access_token
                 tokens["refresh_token"] = refresh_token
                 tokens["access_token"] = self.access_token
@@ -99,7 +112,8 @@ class AsyncClient(Client):
         update_tokens: bool = True,
     ) -> dict:
         if content_type is None:
-            self.headers.pop("Content-Type")
+            if "Content-Type" in self.headers:
+                self.headers.pop("Content-Type")
         else:
             self.headers["Content-Type"] = content_type
         async with aiohttp.ClientSession() as session:
@@ -127,42 +141,50 @@ class AsyncClient(Client):
 
     async def _get_access_token(self, refresh_token: str) -> tuple:
         payload = {"refresh_token": refresh_token}
-        result = await self._apost(
+        response = await self._apost(
             "/oauth/token/refresh", json.dumps(payload), update_tokens=False
         )
-        return result["refresh_token"], result["access_token"]
+        return EmoTokens(**response)
 
     async def get_account_info(self) -> dict:
-        return await self._aget("/v1/me")
+        response = await self._aget("/v1/me")
+        return EmoAccountInfo(**response)
 
     async def delete_account_info(self) -> dict:
-        return await self._adelete("/v1/me")
+        response = await self._adelete("/v1/me")
+        return EmoAccountInfo(**response)
 
     async def get_rooms_list(self) -> dict:
-        return await self._aget("/v1/rooms")
+        response = await self._aget("/v1/rooms")
+        return EmoRoomInfo(**response)
 
     def create_room_client(self, room_id: str):
         return Room(self, room_id)
 
     async def get_stamps_list(self) -> dict:
-        return await self._aget("/v1/stamps")
+        response = await self._aget("/v1/stamps")
+        return EmoStampsInfo(**response)
 
     async def get_motions_list(self) -> dict:
-        return await self._aget("/v1/motions")
+        response = await self._aget("/v1/motions")
+        return EmoMotionsInfo(**response)
 
     async def get_webhook_setting(self) -> dict:
-        return await self._aget("/v1/webhook")
+        response = await self._aget("/v1/webhook")
+        return EmoWebhookInfo(**response)
 
     async def change_webhook_setting(self, webhook: WebHook) -> dict:
         payload = {"description": webhook.description, "url": webhook.url}
-        return await self._aput("/v1/webhook", json.dumps(payload))
+        response = await self._aput("/v1/webhook", json.dumps(payload))
+        return EmoWebhookInfo(**response)
 
     async def delete_webhook_setting(self) -> dict:
-        return await self._adelete("/v1/webhook")
+        response = await self._adelete("/v1/webhook")
+        return EmoWebhookInfo(**response)
 
     def start_webhook_event(self, host: str = "localhost", port: int = 8000) -> None:
         response = self.register_webhook_event(list(self.webhook_events_cb.keys()))
-        secret_key = response["secret"]
+        secret_key = response.secret
 
         app = FastAPI()
 
@@ -190,79 +212,92 @@ class Room:
 
     async def get_msgs(self, ts: int = None) -> dict:
         params = {"before": ts} if ts else {}
-        return await self.base_client._aget(
+        response = await self.base_client._aget(
             "/v1/rooms/" + self.room_id + "/messages", params=params
         )
+        return EmoMsgsInfo(**response)
 
     async def get_sensors_list(self) -> dict:
-        return await self.base_client._aget("/v1/rooms/" + self.room_id + "/sensors")
+        response = await self.base_client._aget(
+            "/v1/rooms/" + self.room_id + "/sensors"
+        )
+        return EmoSensorsInfo(**response)
 
     async def get_sensor_values(self, sensor_id: str) -> dict:
-        return await self.base_client._aget(
+        response = await self.base_client._aget(
             "/v1/rooms/" + self.room_id + "/sensors/" + sensor_id + "/values"
         )
+        return EmoRoomSensorInfo(**response)
 
     async def send_audio_msg(self, audio_data_path: str) -> dict:
         with open(audio_data_path, "rb") as audio_data:
             data = aiohttp.FormData()
             data.add_field("audio", audio_data, content_type="multipart/form-data")
-            return await self.base_client._apost(
+            response = await self.base_client._apost(
                 "/v1/rooms/" + self.room_id + "/messages/audio",
                 data=data,
                 content_type=PostContentType.MULTIPART_FORMDATA,
             )
+            return EmoMessageInfo(**response)
 
     async def send_image(self, image_data_path: str) -> dict:
         with open(image_data_path, "rb") as image_data:
             data = aiohttp.FormData()
             data.add_field("image", image_data, content_type="multipart/form-data")
-            # files = {"image": image_data}
-            return await self.base_client._apost(
+            response = await self.base_client._apost(
                 "/v1/rooms/" + self.room_id + "/messages/image",
                 data=data,
                 content_type=PostContentType.MULTIPART_FORMDATA,
             )
+            return EmoMessageInfo(**response)
 
     async def send_msg(self, msg: str) -> dict:
         payload = {"text": msg}
-        return await self.base_client._apost(
+        response = await self.base_client._apost(
             "/v1/rooms/" + self.room_id + "/messages/text", json.dumps(payload)
         )
+        return EmoMessageInfo(**response)
 
     async def send_stamp(self, stamp_id: str, msg: Optional[str] = None) -> dict:
         payload = {"uuid": stamp_id}
         if msg:
             payload["text"] = msg
-        return await self.base_client._apost(
+        response = await self.base_client._apost(
             "/v1/rooms/" + self.room_id + "/messages/stamp", json.dumps(payload)
         )
+        return EmoMessageInfo(**response)
 
     async def send_original_motion(self, file_path: str) -> dict:
         with open(file_path) as f:
             payload = json.load(f)
-            return await self.base_client._apost(
+            response = await self.base_client._apost(
                 "/v1/rooms/" + self.room_id + "/motions", json.dumps(payload)
             )
+            return EmoMessageInfo(**response)
 
     async def change_led_color(self, color: Color) -> dict:
         payload = {"red": color.red, "green": color.green, "blue": color.blue}
-        return await self.base_client._apost(
+        response = await self.base_client._apost(
             "/v1/rooms/" + self.room_id + "/motions/led_color", json.dumps(payload)
         )
+        return EmoMessageInfo(**response)
 
     async def move_to(self, head: Head) -> dict:
         payload = {"angle": head.angle, "vertical_angle": head.vertical_angle}
-        return await self.base_client._apost(
+        response = await self.base_client._apost(
             "/v1/rooms/" + self.room_id + "/motions/move_to", json.dumps(payload)
         )
+        return EmoMessageInfo(**response)
 
     async def send_motion(self, motion_id: str) -> dict:
         payload = {"uuid": motion_id}
-        return await self.base_client._apost(
+        response = await self.base_client._apost(
             "/v1/rooms/" + self.room_id + "/motions/preset", json.dumps(payload)
         )
+        return EmoMessageInfo(**response)
 
     async def get_emo_settings(self) -> dict:
-        return await self.base_client._aget(
+        response = await self.base_client._aget(
             "/v1/rooms/" + self.room_id + "/emo/settings"
         )
+        return EmoSettingsInfo(**response)
